de5ebf9173ae7f95095df95d6369d9366beb0c97
Mauricio Aldazosa Mariaca <mauricio.aldazosa@ciencias.unam.mx>
2023-05-11 12:19:32 -0600

    0.1.0 Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dai-ichi "0.1.0-SNAPSHOT"
+(defproject dai-ichi "0.1.0"
 
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -44,21 +44,18 @@
                  [thheller/shadow-cljs "2.20.3" :scope "provided"]]
 
   :min-lein-version "2.0.0"
-  
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot dai-ichi.core
 
-  :plugins [] 
+  :plugins []
   :clean-targets ^{:protect false}
   [:target-path "target/cljsbuild"]
-  
 
   :profiles
   {:uberjar {:omit-source true
-             
              :prep-tasks ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
              :aot :all
              :uberjar-name "dai-ichi.jar"
@@ -79,9 +76,8 @@
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
                                  [jonase/eastwood "1.2.4"]
-                                 [cider/cider-nrepl "0.26.0"]] 
-                  
-                  
+                                 [cider/cider-nrepl "0.26.0"]]
+
                   :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
@@ -89,9 +85,6 @@
                   :injections [(require 'pjstadig.humane-test-output)
                                (pjstadig.humane-test-output/activate!)]}
    :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
-                  
-                  
-                  }
+                  :resource-paths ["env/test/resources"]}
    :profiles/dev {}
    :profiles/test {}})
